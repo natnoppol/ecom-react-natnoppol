374c5331c7ccd63ae003fb7ea5d517804811d497
natnoppol <126169607+natnoppol@users.noreply.github.com>
2025-02-08 15:14:36 +0100

    delete cardFooter on card ui
    reason: dont need it

--- src/components/ui/card.jsx
+++ src/components/ui/card.jsx
@@ -41,12 +41,5 @@ const CardContent = React.forwardRef(({ className, ...props }, ref) => (
 ))
 CardContent.displayName = "CardContent"
 
-const CardFooter = React.forwardRef(({ className, ...props }, ref) => (
-  <div
-    ref={ref}
-    className={cn("flex items-center p-6 pt-0", className)}
-    {...props} />
-))
-CardFooter.displayName = "CardFooter"
 
-export { Card, CardHeader, CardFooter, CardTitle, CardDescription, CardContent }
+export { Card, CardHeader, CardTitle, CardDescription, CardContent }
