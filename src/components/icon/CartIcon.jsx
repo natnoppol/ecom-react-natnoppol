85e2b89f60e09dc20f904f3179897accdf73776a
natnoppol <126169607+natnoppol@users.noreply.github.com>
2025-02-19 22:33:44 +0100

    update CartIcon

--- src/components/icon/CartIcon.jsx
+++ src/components/icon/CartIcon.jsx
@@ -1,23 +1,29 @@
-import { Link } from 'react-router-dom';
-import { useCart } from '../../hooks/CartContext';
+import { Link } from "react-router-dom";
+import { useCart } from "../../hooks/CartContext";
 
 function CartIcon() {
   const { cart } = useCart();
   const totalItems = cart.reduce((sum, item) => sum + item.quantity, 0);
 
   return (
-    <Link to="/cart" style={{ position: 'relative', padding: '10px' }}>
+    <Link to="/cart" style={{ position: "relative", padding: "10px", fontSize: "18px" }}>
       🛒 Cart
       {totalItems > 0 && (
         <span
           style={{
-            position: 'absolute',
-            top: 0,
-            right: 0,
-            background: 'red',
-            borderRadius: '50%',
-            padding: '5px',
-            color: 'white',
+            position: "absolute",
+            top: "-5px",
+            right: "-5px",
+            background: "red",
+            borderRadius: "50%",
+            width: "20px",
+            height: "20px",
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "center",
+            color: "white",
+            fontSize: "12px",
+            fontWeight: "bold",
           }}
         >
           {totalItems}
